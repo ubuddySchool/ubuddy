b6c124b65169d612e29244e2e86c993e5ce874c8
ubuddySchool <ubuddyhosting377@gmail.com>
2025-02-26 09:44:05 +0530

    users

--- database/seeders/CreateUsersSeeder.php
+++ database/seeders/CreateUsersSeeder.php
@@ -19,23 +19,23 @@ class CreateUsersSeeder extends Seeder
             [
                'name'=>'Admin',
                'email'=>'superadmin@ubuddyapp.com',
-               'type'=>1,
                'contact_no'=>9876543211,
                'address'=>'Indore',
                'city'=>'Indore',
                'pincode'=>'451010',
-               'username'=>'ubuddyadmin',  // Ensure username is provided
+               'username'=>'ubuddyadmin',  
+               'type'=>1,
                'password'=> bcrypt('Ubuddy$$19'),
             ],
             [
                'name'=>'User',
                'email'=>'user@ubuddy.com',
-               'type'=>0,
                'contact_no'=>0000000000,
                'address'=>'Indore',
                'city'=>'Indore',
                'pincode'=>'451010',
                'username'=>'ubuddyuser',  // Ensure username is provided
+               'type'=>0,
                'password'=> bcrypt('123456'),
             ],
         ];

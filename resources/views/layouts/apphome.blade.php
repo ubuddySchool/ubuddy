f0e4d42b245faccf2c0a28381b942267806c33a5
ubuddySchool <ubuddyhosting377@gmail.com>
2025-04-22 11:58:15 +0530

    changes done

--- resources/views/layouts/apphome.blade.php
+++ resources/views/layouts/apphome.blade.php
@@ -33,7 +33,14 @@
     <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-
+<style>
+    
+.backButton{
+	border: 1px solid #313744;
+    padding: 5px 10px;
+    border-radius: 50%;
+}
+</style>
 </head>
 
 <body>

ac6e233614aeff40282d676aa22990fc0c57b852
ubuddySchool <ubuddyhosting377@gmail.com>
2025-04-25 14:08:08 +0530

    crm filteration work completed done

--- resources/views/user/enquiry/visit_record.blade.php
+++ resources/views/user/enquiry/visit_record.blade.php
@@ -1,91 +1,73 @@
 @extends('layouts.apphome')
 
 @section('content')
-
+<div class="content container-fluid">
 <div class="row">
     <div class="col-sm-12">
         <div class="card card-table">
             <div class="card-body">
 
-                <div class="page-header">
-                    <div class="row align-items-center justify-content-between">
-                        <div class="col-12 col-md-5 mb-3 mb-md-0">
-                            <h3 class="page-title">Visit List</h3>
+                    <div class="page-header">
+                        <div class="row align-items-center">
+                            <div class="col align-items-center">
+                                <a href="{{ route('home') }}" class="text-decoration-none text-dark me-2 backButton">
+                                    <i class="fas fa-arrow-left"></i>
+                                </a>
+                                <h3 class="page-title">Visit List</h3>
+                            </div>
                         </div>
+                    </div>
 
+                <div class="page-header">
+                    <div class="row align-items-center justify-content-between gap-3">
+                        
 
-                        <div class="col-12 col-md-auto mb-3 mb-md-0">
-                            <form method="GET" action="{{ route('visit_record') }}">
+                        <div class="col-12 col-md-12 mb-3 mb-md-0">
+                            <!-- Filter Form -->
+                            <form id="filterForm">
+                           
                                 <div class="d-flex flex-column flex-md-row align-items-center gap-3 gap-md-2 justify-content-between">
-                                    <div class="d-flex align-items-center">
-                                        <label for="from_date" class="form-label mb-0  me-1">From:</label>
+                                <div class="col-md-3 d-flex align-items-center gap-2">
                                         <input type="date" id="from_date" name="from_date" class="form-control form-control-sm"
                                             value="{{ request('from_date') }}">
-                                    </div>
                                     <div class="d-flex align-items-center">
-                                        <label for="to_date" class="form-label mb-0 me-1">To:</label>
+                                        <label for="to_date" class="form-label mb-0 me-1">To</label>
                                         <input type="date" id="to_date" name="to_date" class="form-control form-control-sm"
                                             value="{{ request('to_date') }}">
                                     </div>
                                 </div>
+
+                                    <div class="col-md-3">
+                                        <select id="contact_method" name="contact_method" class="form-select form-control-sm">
+                                            <option value="">Visit Type</option>
+                                            <option value="1" {{ request('contact_method') == '1' ? 'selected' : '' }}>In-person</option>
+                                            <option value="0" {{ request('contact_method') == '0' ? 'selected' : '' }}>Telephonic</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <select id="visit_type" name="visit_type" class="form-select form-control-sm">
+                                            <option value="">Meeting Type</option>
+                                            <option value="New Meeting" {{ request('visit_type') == 'New Meeting' ? 'selected' : '' }}>New Meeting</option>
+                                            <option value="Follow-up" {{ request('visit_type') == 'Follow-up' ? 'selected' : '' }}>Follow-up</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input mb-0" type="checkbox" name="today_visit" value="today" id="today_visit"
+                                               {{ request('today_visit') == 'today' ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="today_visit">Today's Visits Only</label>
+                                    </div>
+                                    </div>
+                                </div>
                             </form>
                         </div>
 
-                        <!-- Today's / All Time Filter -->
-                        <div class="col-12 col-md-auto mb-3 mb-md-0">
-                            <form method="GET" action="{{ route('visit_record') }}">
-                                <input type="checkbox"
-                                    id="expiry_filter_switch"
-                                    class="form-check-input"
-                                    name="today_visit"
-                                    value="today"
-                                    onchange="this.form.submit()"
-                                    {{ request('today_visit') == 'showall' ? 'checked' : '' }}>
-                                <label for="expiry_filter_switch" class="form-label mb-0">Show all Visits</label>
-                            </form>
-                        </div>
-
-
-                        <!-- Back Button Section -->
-                        <div class="col-12 col-md-auto mb-3 mb-md-0">
-                            <a href="{{ route('home') }}" class="btn btn-primary btn-sm w-100 w-md-auto">Back</a>
-                        </div>
-
-
-
-
-
-                    </div>
-                </div>
-                <div class="page-header">
-                    <div class="row align-items-center justify-content-between">
-                        <div class="col-12 col-md-6 mb-3 mb-md-0">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <select id="contact_method" name="contact_method" class="form-select form-control-sm">
-                                        <option value="">Visit type</option>
-                                        <option value="New Meeting" {{ request('contact_method') == '0' ? 'selected' : '' }}>In-person</option>
-                                        <option value="Follow-up" {{ request('contact_method') == '1' ? 'selected' : '' }}>Telephonic</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-6">
-                                    <select id="visit_type" name="visit_type" class="form-select form-control-sm">
-                                        <option value="">Meeting type</option>
-                                        <option value="New Meeting" {{ request('visit_type') == 'New Meeting' ? 'selected' : '' }}>New Meeting</option>
-                                        <option value="Follow-up" {{ request('visit_type') == 'Follow-up' ? 'selected' : '' }}>Follow-up</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-auto text-center text-md-end">
-                            <button class="btn btn-primary my-3" disabled>
-                            No. of Visits:
-                                {{ $enquiries->sum(fn($enquiry) => $enquiry->visits->count() ?? 0) }}
-                            </button>
-                        </div>
                     </div>
                 </div>
 
+                <!-- Data Table -->
                 <div class="response mt-3">
                     <table class="table border-0 star-student table-hover table-center mb-0 datatable table-responsive table-striped" id="enquiry-table">
                         <thead class="student-thread">
@@ -99,46 +81,7 @@
                             </tr>
                         </thead>
                         <tbody id="table-body">
-                            @php $rowNumber = 1; @endphp
-
-                            @if ($enquiries->isEmpty() || $enquiries->every(fn($enquiry) => $enquiry->visits->isEmpty()))
-                            <tr>
-                                <td colspan="6" class="text-center">No data available</td>
-                            </tr>
-                            @else
-                            @foreach ($enquiries as $enquiry)
-                            @foreach ($enquiry->visits as $visit)
-                            <tr>
-                                <td>{{ $rowNumber++ }}</td>
-                                <td>{{ $enquiry->school_name ?? 'No School Name' }}</td>
-                                <td>{{ \Carbon\Carbon::parse($visit->date_of_visit)->format('d-m-Y') }}</td>
-                                <td>
-                                    @if ($visit->contact_method == 1)
-                                    In Person Meeting
-                                    @elseif ($visit->contact_method == 0)
-                                    Telephonic
-                                    @else
-                                    N/A
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($visit->visit_type == 1)
-                                    New Meeting
-                                    @elseif ($visit->visit_type == 0)
-                                    Follow-up
-                                    @else
-                                    N/A
-                                    @endif
-                                </td>
-                                <td>
-                                    <a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#view-modal{{ $enquiry->id  }}">
-                                        View Details
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                            @endforeach
-                            @endif
+                            <!-- Data will be populated via AJAX -->
                         </tbody>
                     </table>
                 </div>
@@ -149,6 +92,49 @@
 </div>
 </div>
 
-@include('user.modal')
+<script>
+    $(document).ready(function() {
+        $('#filterForm input, #filterForm select').on('change', function() {
+            loadFilteredData();
+        });
 
+        function loadFilteredData() {
+            $.ajax({
+                url: "{{ route('visit_record') }}",
+                type: 'GET',
+                data: $('#filterForm').serialize(), 
+                success: function(response) {
+                    $('#table-body').empty();
+
+                    if (response.enquiries.length > 0) {
+                        let rowNumber = response.rowNumber;
+                        $.each(response.enquiries, function(index, enquiry) {
+                            $.each(enquiry.visits, function(i, visit) {
+                                let visitType = (visit.visit_type == 1) ? 'New Meeting' : 'Follow-up';
+                                let contactMethod = (visit.contact_method == 1) ? 'In-person' : 'Telephonic';
+                                $('#table-body').append(`
+                                    <tr>
+                                        <td>${rowNumber++}</td>
+                                        <td>${enquiry.school_name || 'No School Name'}</td>
+                                        <td>${visit.date_of_visit}</td>
+                                        <td>${contactMethod}</td>
+                                        <td>${visitType}</td>
+                                        <td><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#view-modal${enquiry.id}">View Details</a></td>
+                                    </tr>
+                                `);
+                            });
+                        });
+                    } else {
+                        $('#table-body').append('<tr><td colspan="6" class="text-center">No data available</td></tr>');
+                    }
+                },
+                error: function() {
+                    alert('Failed to load data.');
+                }
+            });
+        }
+
+        loadFilteredData();
+    });
+</script>
 @endsection
--- resources/views/user/followup/index.blade.php
+++ resources/views/user/followup/index.blade.php
@@ -1,36 +1,30 @@
 @extends('layouts.apphome')
 
 @section('content')
-<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 
-@if(session('success'))
-<script>
-    toastr.success("{{ session('success') }}");
-</script>
-@endif
-
-@if(session('error'))
-<script>
-    toastr.error("{{ session('error') }}");
-</script>
-@endif
-
-
+<div class="content container-fluid">
 <div class="row">
     <div class="col-sm-12">
         <div class="card card-table">
             <div class="card-body">
 
-                <div class="page-header">
-                    <a href="{{ route('home') }}" class="btn btn-primary float-end btn-sm">Back</a>
-                    <div class="row align-items-center">
-                        <div class="col-12 col-md-6">
-                            <h3 class="page-title">Follow up List</h3>
+            <div class="page-header">
+                        <div class="row align-items-center">
+                            <div class="col align-items-center">
+                                <a href="{{ route('home') }}" class="text-decoration-none text-dark me-2 backButton">
+                                    <i class="fas fa-arrow-left"></i>
+                                </a>
+                                <h3 class="page-title">Follow-up List</h3>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-6 text-end float-end ms-auto download-grp">
-                            <form method="GET" action="{{ route('follow_up') }}">
+                    </div>
+
+                <div class="page-header">
+                    <div class="row align-items-center">
+                       
+                        <div class="col-12 col-md-12 ms-auto download-grp">
+                            <form id="filterForm">
                                 <div class="d-flex flex-column flex-md-row align-items-center gap-2">
                                     <div class="d-flex align-items-center gap-2">
                                         <label for="from_date" class="form-label mb-0">From:</label>
@@ -43,87 +37,102 @@
                                         <input type="date" id="to_date" name="to_date" class="form-control form-control-sm"
                                             value="{{ request('to_date') }}">
                                     </div>
-
-                                    <!-- <div class="d-flex gap-2">
-                                        <button type="submit" class="btn btn-info  btn-sm">Filter</button>
-                                    </div> -->
                                 </div>
                             </form>
                         </div>
                     </div>
-
-
-
-                    <div class="response mt-3">
-                        <table class="table border-0 star-student table-hover table-center mb-0 datatable table-responsive table-striped" id="enquiry-table">
-                            <thead class="student-thread">
-                                <tr>
-                                    <th>S No.</th>
-                                    <th>School Name</th>
-                                    <th>Last Visit Date</th>
-                                    <th>Remarks</th>
-                                    <th>Follow Up</th>
-                                </tr>
-                            </thead>
-                            <tbody id="table-body">
-                                @if($noDataFound)
-                                <tr>
-                                    <td colspan="4" class="text-center text-muted">No Data Found</td>
-                                </tr>
-                                @else
-                                @php $serial = 1; @endphp
-                                @foreach ($enquiries as $enquiry)
-                                @foreach ($enquiry->visits as $visit)
-                                <tr>
-                                    <td>{{ $serial++ }}</td>
-                                    <td>{{ $enquiry->school_name ?? 'No School Name' }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($visit->date_of_visit)->format('d-m-Y') }}</td>
-                                    <td>Remarks</td>
-                                    <td>{{ $visit->follow_up_date }}</td>
-                                </tr>
-                                @endforeach
-                                @endforeach
-                                @endif
-                            </tbody>
-
-                        </table>
-                    </div>
                 </div>
+
+                <!-- Data Table -->
+                <div class="response mt-3">
+                    <table class="table border-0 star-student table-hover table-center mb-0 datatable table-responsive table-striped" id="enquiry-table">
+                        <thead class="student-thread">
+                            <tr>
+                                <th>S No.</th>
+                                <th>School Name</th>
+                                <th>Last Visit Date</th>
+                                <th>Remarks</th>
+                                <th>Follow Up</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table-body">
+                            @if($noDataFound)
+                            <tr>
+                                <td colspan="5" class="text-center text-muted">No Data Found</td>
+                            </tr>
+                            @else
+                            @php $serial = 1; @endphp
+                            @foreach ($enquiries as $enquiry)
+                            @foreach ($enquiry->visits as $visit)
+                            <tr>
+                                <td>{{ $serial++ }}</td>
+                                <td>{{ $enquiry->school_name ?? 'No School Name' }}</td>
+                                <td>{{ \Carbon\Carbon::parse($visit->date_of_visit)->format('d-m-Y') }}</td>
+                                <td>Remarks</td>
+                                <td>{{ $visit->follow_up_date }}</td>
+                            </tr>
+                            @endforeach
+                            @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     </div>
 </div>
+</div>
 
-
-@include('user.modal')
-
-
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-    //    $(document).ready(function() {
+ $(document).ready(function() {
+    // Listen for changes in the filter form
+    $('#filterForm input, #filterForm select').on('change', function() {
+        loadFilteredData();
+    });
 
-    //     $('#expiry_filter_switch').change(function() {
-    //         var filterValue = $(this).prop('checked') ? 'expired' : 'not_expired'; 
-    //         updateTable(filterValue); 
-    //     });
+    // Load filtered data via AJAX
+    function loadFilteredData() {
+        $.ajax({
+            url: "{{ route('visit_record') }}",  // The route to fetch filtered data
+            type: 'GET',
+            data: $('#filterForm').serialize(), // Serialize the form data (including dates)
+            success: function(response) {
+                $('#table-body').empty();
 
-    //     function updateTable(filterValue) {
-    //         $.ajax({
-    //             url: '', 
-    //             type: 'GET',
-    //             data: { 
-    //                 expiry_filter: filterValue 
-    //             },
-    //             success: function(response) {
-    //                 $('#table-body').html(response.html); 
-    //             },
-    //             error: function(xhr, status, error) {
-    //                 console.error("Error fetching filtered data:", error);
-    //             }
-    //         });
-    //     }
-    // });
+                if (response.enquiries.length > 0) {
+                    let rowNumber = response.rowNumber;
+                    $.each(response.enquiries, function(index, enquiry) {
+                        $.each(enquiry.visits, function(i, visit) {
+                            let visitType = (visit.visit_type == 1) ? 'New Meeting' : 'Follow-up';
+                            let contactMethod = (visit.contact_method == 1) ? 'In-person' : 'Telephonic';
+                            $('#table-body').append(`
+                                <tr>
+                                    <td>${rowNumber++}</td>
+                                    <td>${enquiry.school_name || 'No School Name'}</td>
+                                    <td>${visit.date_of_visit}</td>
+                                    <td>${contactMethod}</td>
+                                    <td>${visitType}</td>
+                                    <td><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#view-modal${enquiry.id}">View Details</a></td>
+                                </tr>
+                            `);
+                        });
+                    });
+                } else {
+                    $('#table-body').append('<tr><td colspan="6" class="text-center">No data available</td></tr>');
+                }
+            },
+            error: function() {
+                alert('Failed to load data.');
+            }
+        });
+    }
+
+    loadFilteredData();
+});
+
+    
 </script>
+
 
 @endsection
